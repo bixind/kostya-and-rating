a9fda3fa49ee86ddc9f77d03fd90425f7cf40e70
bixind <bixind@users.noreply.github.com>
2015-05-25 21:44:55 +0400

    В городе новый вывод

--- InstRoamer/main.py
+++ InstRoamer/main.py
@@ -1,25 +1,57 @@
+from tkinter import *
 from InstUser import *
-from datetime import *
-d=datetime.today()
+#from datetime import *
 
-fout = open(d.strftime('%y_%m_%d')+'.txt', 'w')
-rated_users =[]
-with open('usernames.txt', 'r') as fin:
-    for name in fin:
-        name = name.split()[0].lower()
-        user = InstagramUser(name)
-        if user.username is None:
-            print('Error', name)
-            continue
-        for i in user.getInfo():
-            print(i, end = ' ')
-            print(i, end = ' ', file = fout)
-        print()
-        print(file = fout)
-        rated_users.append(user.getOnlyRate())
-        
-rated_users.sort(key = lambda userinfo: userinfo[1])
-with open('rating.txt','w') as frating:
-    for sorted_user in rated_users:
-        print(sorted_user[0], sorted_user[1], file = frating)
-fout.close()
+root = Tk()
+
+root.title('InstRoamer')
+greeting = Label(root, text = 'Welcome to InstRoamer (prealpha)!')
+greeting.pack(side = 'top')
+checkbox_state = IntVar()
+checkbox = Checkbutton(root, text = 'Print rating to the file', variable = checkbox_state)
+
+def start():
+    global is_rating_saved
+    is_rating_saved = checkbox_state.get()
+    root.destroy()
+    global analyse_window
+    global output
+    analyse_window = Tk()
+    analyse_window.title('InstRoamer')
+    output = Text(analyse_window)
+    output.pack()
+    analyse_window.after(500, analyse)
+    analyse_window.mainloop()
+
+start_button = Button(root, text = 'Start!', command = start)
+start_button.pack(side = 'bottom')
+checkbox.pack(side = 'top')
+
+def analyse():
+    #d=datetime.today()
+    #fout = open(d.strftime('%y_%m_%d')+'.txt', 'w')
+    rated_users =[]
+    with open('usernames.txt', 'r') as fin:
+        for name in fin:
+            name = name.split()[0].lower()
+            user = InstagramUser(name)
+            if user.username is None:
+                output.insert('0.0', 'Error '+ name + '\n')
+                continue
+            output_str = ''
+            for i in user.getInfo():
+                output_str += str(i) + ' '
+            output.insert('0.0', output_str + '\n')
+                #print(i, end = ' ', file = fout)
+            #print(file = fout)
+            rated_users.append(user.getOnlyRate())
+            analyse_window.update()
+    if is_rating_saved:
+        rated_users.sort(key = lambda userinfo: userinfo[1])
+        with open('rating.txt','w') as frating:
+            for sorted_user in rated_users:
+                print(sorted_user[0], sorted_user[1], file = frating)
+    #fout.close()
+    output.insert('0.0', 'Analyse over\n')
+root.mainloop()
+
