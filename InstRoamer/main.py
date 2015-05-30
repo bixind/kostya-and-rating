7b881f17fd2a671569e9b0eaa58bf92edbb31c6b
bixind <bixind@users.noreply.github.com>
2015-05-30 13:57:54 +0400

    Убрал фризы

--- InstRoamer/main.py
+++ InstRoamer/main.py
@@ -1,6 +1,5 @@
-from tkinter import *
-from InstUser import *
 from InstUserTable import *
+import threading
 #from datetime import *
 
 root = Tk()
@@ -12,17 +11,37 @@ greetingLabel = Label(root, text = 'Welcome to Instagram Roamer!', font = '30')
 def analyse():
     waitLabel.pack(side = 'top')
     startButton.pack_forget()
-    rated_users =[]
+    namesList = []
     with open('usernames.txt', 'r') as fin:
         for name in fin:
-            name = name.split()[0].lower()
-            root.update()
-            table.add_user(name)
-            root.update()
-            for i in table.users[table.users.__len__() - 1].getInfo():
-                print(i, end = ' ')
-            print()
-            table.show_slaves()
+            namesList.append(name.split()[0].lower())
+    namesLock = threading.Lock()
+    tableLock = threading.Lock()
+
+    def connect():
+        while True:
+            namesLock.acquire()
+            if (namesList.__len__() < 1):
+                namesLock.release()
+                return
+            name = namesList.pop()
+            namesLock.release()
+            userLabel = UserLabel(name = name, master=table)
+            tableLock.acquire()
+            table.add_label(userLabel)
+            tableLock.release()
+
+    thrCount = 30
+    for i in range(thrCount):
+        thread = threading.Thread(target=connect)
+        thread.start()
+
+    while threading.active_count() > 1:
+        root.update()
+    for i in table.users[table.users.__len__() - 1].getInfo():
+        print(i, end = ' ')
+    print()
+    table.show_slaves()
     waitLabel.pack_forget()
     errorCountLabel['text'] = 'Errors: ' + str(table.errorCount)
     errorCountLabel.pack(side = 'top')
